bb1005dc0604dc6b7fea0d0d31b49e73c982e4c0
scooter7 <scooter.vineburgh@gmail.com>
2023-07-25 21:44:49 -0500

    Update ads3.py

--- ads3.py
+++ ads3.py
@@ -1,14 +1,14 @@
-import os
 from PIL import Image, ImageDraw, ImageFont
 import streamlit as st
 import base64
 from io import BytesIO
 
+DEFAULT_FONT_PATH = ImageFont.load_default()
+
 def merge_text_with_image(image, text, font_size, text_color, bg_color, position, position_mapping):
     img = image.copy()
     draw = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype("arial.ttf", font_size)
+    font = ImageFont.truetype(DEFAULT_FONT_PATH, font_size)
     text_width, text_height = draw.textsize(text, font=font)
 
     if position == "center":
@@ -21,7 +21,6 @@ def merge_text_with_image(image, text, font_size, text_color, bg_color, position
         else:
             x, y = position_mapping[position]
 
-        # Convert x and y coordinates to integers, if possible
         try:
             x = int(x)
         except (ValueError, TypeError):
@@ -32,11 +31,11 @@ def merge_text_with_image(image, text, font_size, text_color, bg_color, position
         except (ValueError, TypeError):
             y = None
 
-    if x is None:  # Handle "center" position for x-coordinate
+    if x is None:
         img_width, _ = img.size
         x = (img_width - text_width) // 2
 
-    if y is None:  # Handle "center" position for y-coordinate
+    if y is None:
         _, img_height = img.size
         y = (img_height - text_height) // 2
 
@@ -63,26 +62,12 @@ def main():
     num_texts = st.number_input("Enter the number of text fields", min_value=1, step=1)
     texts = [st.text_input(f"Enter text {i + 1}") for i in range(num_texts)]
 
-    text_colors = []
-    text_color_black = st.checkbox("Text Color (Black)")
-    if text_color_black:
-        text_colors.append((0, 0, 0))
-    text_color_white = st.checkbox("Text Color (White)")
-    if text_color_white:
-        text_colors.append((255, 255, 255))
+    text_colors = [(0, 0, 0) if st.checkbox("Text Color (Black)") else (255, 255, 255) if st.checkbox("Text Color (White)") else None]
 
-    bg_colors = [None]  # Add None for the "No Background" option
-    bg_color_black = st.checkbox("Text Background Area (Black)")
-    if bg_color_black:
-        bg_colors.append((0, 0, 0))
-    bg_color_white = st.checkbox("Text Background Area (White)")
-    if bg_color_white:
-        bg_colors.append((255, 255, 255))
+    bg_colors = [None]
+    bg_colors += [(0, 0, 0) if st.checkbox("Text Background Area (Black)") else (255, 255, 255) if st.checkbox("Text Background Area (White)") else None]
 
-    font_sizes = []
-    num_font_sizes = st.number_input("Enter the number of font sizes", min_value=1, step=1)
-    for i in range(num_font_sizes):
-        font_sizes.append(st.slider(f"Text Font Size {i + 1}", 10, 100, 40, step=1))
+    font_sizes = [st.slider(f"Text Font Size {i + 1}", 10, 100, 40, step=1) for i in range(st.number_input("Enter the number of font sizes", min_value=1, step=1))]
 
     positions = ["top-left", "top-center", "top-right", "bottom-left", "bottom-center", "bottom-right", "center"]
     selected_positions = [position for position in positions if st.checkbox(position.title())]
@@ -100,14 +85,14 @@ def main():
     selected_image_sizes = [size for size, selected in image_sizes.items() if st.checkbox(size)]
 
     position_mapping = {
-    "top-left": (10, 10),
-    "top-center": ("center", 10),
-    "top-right": (None, 10),
-    "bottom-left": (10, None),
-    "bottom-center": ("center", None),
-    "bottom-right": (None, None),
-    "center": ("center", "center"),
-}
+        "top-left": (10, 10),
+        "top-center": ("center", 10),
+        "top-right": (None, 10),
+        "bottom-left": (10, None),
+        "bottom-center": ("center", None),
+        "bottom-right": (None, None),
+        "center": ("center", "center"),
+    }
 
     if st.button("Merge and Download"):
         if uploaded_images:
