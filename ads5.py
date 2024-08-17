bd29d780ffc69e3d0c233495803b463571fce4ae
scooter7 <scooter.vineburgh@gmail.com>
2024-08-17 18:22:07 -0500

    Update ads5.py

--- ads5.py
+++ ads5.py
@@ -22,6 +22,33 @@ def calculate_font_size(draw, text, img_width, img_height, width_percentage, hei
 
     return font_size - 1
 
+def merge_text_with_image(image, call_to_action_text, description_text, width_percentages, height_percentages, text_colors, bg_colors, cta_position, desc_position, logo_position, logo_width_percentage, logo_height_percentage, uploaded_logo):
+    img = image.copy()
+    draw = ImageDraw.Draw(img)
+    img_width, img_height = img.size
+
+    font_size_cta = calculate_font_size(draw, call_to_action_text, img_width, img_height, width_percentages[0], height_percentages[0])
+    font_size_desc = calculate_font_size(draw, description_text, img_width, img_height, width_percentages[1], height_percentages[1])
+    font_cta = ImageFont.truetype(DEFAULT_FONT_PATH, font_size_cta)
+    font_desc = ImageFont.truetype(DEFAULT_FONT_PATH, font_size_desc)
+
+    text_width_cta, text_height_cta = draw.textsize(call_to_action_text, font=font_cta)
+    x_cta, y_cta = get_position_coordinates(cta_position, img_width, img_height, text_width_cta, text_height_cta)
+
+    text_width_desc, text_height_desc = draw.textsize(description_text, font=font_desc)
+    x_desc, y_desc = get_position_coordinates(desc_position, img_width, img_height, text_width_desc, text_height_desc)
+
+    draw.rectangle([x_cta, y_cta, x_cta + text_width_cta, y_cta + text_height_cta], fill=bg_colors[0])
+    draw.text((x_cta, y_cta), call_to_action_text, font=font_cta, fill=text_colors[0])
+
+    draw.rectangle([x_desc, y_desc, x_desc + text_width_desc, y_desc + text_height_desc], fill=bg_colors[1])
+    draw.text((x_desc, y_desc), description_text, font=font_desc, fill=text_colors[1])
+
+    if logo_position != cta_position and logo_position != desc_position:
+        img = overlay_logo(img, uploaded_logo, logo_position, img_width, img_height, logo_width_percentage, logo_height_percentage)
+
+    return img
+
 def get_position_coordinates(position, img_width, img_height, text_width, text_height, y_offset=0):
     if position == "top-left":
         x = 10
@@ -67,33 +94,6 @@ def overlay_logo(image, uploaded_logo, logo_position, img_width, img_height, log
 
     return img.convert("RGB")
 
-def merge_text_with_image(image, call_to_action_text, description_text, width_percentages, height_percentages, text_colors, bg_colors, cta_position, desc_position, logo_position, logo_width_percentage, logo_height_percentage, uploaded_logo):
-    img = image.copy()
-    draw = ImageDraw.Draw(img)
-    img_width, img_height = img.size
-
-    font_size_cta = calculate_font_size(draw, call_to_action_text, img_width, img_height, width_percentages[0], height_percentages[0])
-    font_size_desc = calculate_font_size(draw, description_text, img_width, img_height, width_percentages[1], height_percentages[1])
-    font_cta = ImageFont.truetype(DEFAULT_FONT_PATH, font_size_cta)
-    font_desc = ImageFont.truetype(DEFAULT_FONT_PATH, font_size_desc)
-
-    text_width_cta, text_height_cta = draw.textsize(call_to_action_text, font=font_cta)
-    x_cta, y_cta = get_position_coordinates(cta_position, img_width, img_height, text_width_cta, text_height_cta)
-
-    text_width_desc, text_height_desc = draw.textsize(description_text, font=font_desc)
-    x_desc, y_desc = get_position_coordinates(desc_position, img_width, img_height, text_width_desc, text_height_desc)
-
-    draw.rectangle([x_cta, y_cta, x_cta + text_width_cta, y_cta + text_height_cta], fill=bg_colors[0])
-    draw.text((x_cta, y_cta), call_to_action_text, font=font_cta, fill=text_colors[0])
-
-    draw.rectangle([x_desc, y_desc, x_desc + text_width_desc, y_desc + text_height_desc], fill=bg_colors[1])
-    draw.text((x_desc, y_desc), description_text, font=font_desc, fill=text_colors[1])
-
-    if logo_position != cta_position and logo_position != desc_position:
-        img = overlay_logo(img, uploaded_logo, logo_position, img_width, img_height, logo_width_percentage, logo_height_percentage)
-
-    return img
-
 def download_images(images_with_text, selected_image_sizes):
     for idx, image in enumerate(images_with_text):
         for channel, label, dimensions in selected_image_sizes:
@@ -109,56 +109,51 @@ def download_images(images_with_text, selected_image_sizes):
 def add_draggable_functionality(img_base64, call_to_action_text, description_text, logo_base64, img_width, img_height):
     # HTML and JS for draggable elements
     st.components.v1.html(f"""
-        <style>
-            .draggable {{
-                position: absolute;
-                cursor: move;
-            }}
-            #logoImage img {{
-                width: 100px; height: auto;
-            }}
-        </style>
         <div style="position: relative; width: {img_width}px; height: {img_height}px; background-image: url('data:image/png;base64,{img_base64}'); background-size: contain; background-repeat: no-repeat;">
-            <div id="ctaText" class="draggable" style="top: 50px; left: 50px; font-size: 24px; color: white;">
+            <div id="ctaText" style="position: absolute; top: 50px; left: 50px; cursor: move; font-size: 24px; color: white;">
                 {call_to_action_text}
             </div>
-            <div id="descText" class="draggable" style="top: 150px; left: 50px; font-size: 18px; color: yellow;">
+            <div id="descText" style="position: absolute; top: 150px; left: 50px; cursor: move; font-size: 18px; color: yellow;">
                 {description_text}
             </div>
-            <div id="logoImage" class="draggable" style="top: 250px; left: 50px;">
-                <img src="data:image/png;base64,{logo_base64}">
+            <div id="logoImage" style="position: absolute; top: 250px; left: 50px; cursor: move;">
+                <img src="data:image/png;base64,{logo_base64}" style="width: 100px; height: auto;">
             </div>
+            <input type="hidden" id="ctaPos" name="ctaPos">
+            <input type="hidden" id="descPos" name="descPos">
+            <input type="hidden" id="logoPos" name="logoPos">
         </div>
 
         <script>
-            document.querySelectorAll('.draggable').forEach(elmnt => {{
-                elmnt.onmousedown = function(event) {{
-                    event.preventDefault();
-                    let shiftX = event.clientX - elmnt.getBoundingClientRect().left;
-                    let shiftY = event.clientY - elmnt.getBoundingClientRect().top;
-                    
-                    function moveAt(pageX, pageY) {{
-                        elmnt.style.left = pageX - shiftX + 'px';
-                        elmnt.style.top = pageY - shiftY + 'px';
-                    }}
-
-                    function onMouseMove(event) {{
-                        moveAt(event.pageX, event.pageY);
-                    }}
-
-                    document.addEventListener('mousemove', onMouseMove);
-
-                    elmnt.onmouseup = function() {{
-                        document.removeEventListener('mousemove', onMouseMove);
-                        elmnt.onmouseup = null;
+            function dragElement(elmnt, posInputId) {{
+                var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+                elmnt.onmousedown = function(e) {{
+                    e = e || window.event;
+                    e.preventDefault();
+                    pos3 = e.clientX;
+                    pos4 = e.clientY;
+                    document.onmouseup = closeDragElement;
+                    document.onmousemove = function(e) {{
+                        e.preventDefault();
+                        pos1 = pos3 - e.clientX;
+                        pos2 = pos4 - e.clientY;
+                        pos3 = e.clientX;
+                        pos4 = e.clientY;
+                        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
                     }};
                 }};
-                
-                // Disable drag and drop on mobile devices
-                elmnt.ontouchstart = function() {{
-                    return false;
-                }};
-            }});
+
+                function closeDragElement() {{
+                    document.onmouseup = null;
+                    document.onmousemove = null;
+                    document.getElementById(posInputId).value = elmnt.style.top + "," + elmnt.style.left;
+                }}
+            }}
+
+            dragElement(document.getElementById("ctaText"), "ctaPos");
+            dragElement(document.getElementById("descText"), "descPos");
+            dragElement(document.getElementById("logoImage"), "logoPos");
         </script>
     """, height=img_height + 50)
 
@@ -167,6 +162,13 @@ def main():
 
     uploaded_images = st.file_uploader("Upload multiple images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
     uploaded_logo = st.file_uploader("Upload logo image", type=["jpg", "jpeg", "png"])
+
+    # Preprocess the logo once
+    if uploaded_logo:
+        logo = Image.open(uploaded_logo)
+        buffered_logo = BytesIO()
+        logo.save(buffered_logo, format="PNG")
+        logo_base64 = base64.b64encode(buffered_logo.getvalue()).decode()
 
     if uploaded_images:
         st.write("Images uploaded successfully!")
@@ -177,21 +179,95 @@ def main():
         st.write("Logo uploaded successfully!")
         st.image(uploaded_logo, caption="Uploaded Logo", use_column_width=True)
 
-    if st.button("Merge and Download"):
-        if uploaded_images and uploaded_logo:
-            st.write("Starting image processing...")
-            try:
-                # Call your image processing functions here
-                process_images(uploaded_images, uploaded_logo)
-                st.write("Images processed and available for download!")
-            except Exception as e:
-                st.error(f"An error occurred: {e}")
-        else:
-            st.error("Please upload both images and a logo before merging.")
+    num_pairs = st.number_input("Number of Call to Action + Description Pairs", min_value=1, step=1)
+    call_to_action_texts = [st.text_input(f"Call to Action Text {i + 1}") for i in range(num_pairs)]
+    description_texts = [st.text_input(f"Description Text {i + 1}") for i in range(num_pairs)]
+    
+    width_percentage_cta = st.slider("Call to Action Width (Percentage of Image Width)", 1, 100, 50, step=1) / 100.0
+    height_percentage_cta = st.slider("Call to Action Height (Percentage of Image Height)", 1, 100, 10, step=1) / 100.0
+    width_percentage_desc = st.slider("Description Width (Percentage of Image Width)", 1, 100, 50, step=1) / 100.0
+    height_percentage_desc = st.slider("Description Height (Percentage of Image Height)", 1, 100, 10, step=1) / 100.0
 
-def process_images(images, logo):
-    # Dummy function for image processing logic
-    st.write("Processing images...")  # Add real processing logic here
+    selected_cta_positions = st.multiselect("Select Call to Action Text Positions", ["top-left", "top-center", "top-right", "middle-left", "middle-center", "middle-right", "bottom-left", "bottom-center", "bottom-right"])
+    selected_desc_positions = st.multiselect("Select Description Text Positions", ["top-left", "top-center", "top-right", "middle-left", "middle-center", "middle-right", "bottom-left", "bottom-center", "bottom-right"])
+
+    call_to_action_text_color = st.color_picker("Call to Action Text Color", "#FFFFFF")
+    call_to_action_bg_color = st.color_picker("Call to Action Background Color", "#000000")
+    description_text_color = st.color_picker("Description Text Color", "#FFFFFF")
+    description_bg_color = st.color_picker("Description Background Color", "#000000")
+
+    logo_width_percentage = st.slider("Logo Width (Percentage of Image Width)", 1, 100, 20, step=1) / 100.0
+    logo_height_percentage = st.slider("Logo Height (Percentage of Image Height)", 1, 100, 20, step=1) / 100.0
+    selected_logo_positions = st.multiselect("Select Logo Positions", ["top-left", "top-center", "top-right", "middle-left", "middle-center", "middle-right", "bottom-left", "bottom-center", "bottom-right"])
+
+    image_sizes = {
+        "IP Targeting": {
+            "300x250": (300, 250),
+            "728x90": (728, 90),
+        },
+        "Mobile Footprinting": {
+            "300x250": (300, 250),
+            "728x90": (728, 90),
+        },
+        "Audience Select": {
+            "300x250": (300, 250),
+            "728x90": (728, 90),
+        },
+        "Spotify": {
+            "640x640": (640, 640),
+            "300x250": (300, 250),
+        },
+        "YouTube": {
+            "1280x720": (1280, 720),
+            "300x250": (300, 250),
+        },
+    }
+
+    selected_image_sizes = []
+    for channel, sizes in image_sizes.items():
+        with st.expander(f"{channel}"):
+            st.write(f"Select ad sizes for {channel}:")
+            for label, dimensions in sizes.items():
+                if st.checkbox(label, key=f"{channel}_{label}"):
+                    selected_image_sizes.append((channel, label, dimensions))
+
+    if st.button("Merge and Download"):
+        if uploaded_images and selected_cta_positions and selected_desc_positions and selected_logo_positions:
+            st.write("Processing images...")
+            images_with_text = []
+            for image in uploaded_images:
+                img = Image.open(image)
+                for cta_position in selected_cta_positions:
+                    for desc_position in selected_desc_positions:
+                        for logo_position in selected_logo_positions:
+                            if cta_position != desc_position and cta_position != logo_position and desc_position != logo_position:
+                                for call_to_action_text, description_text in zip(call_to_action_texts, description_texts):
+                                    merged_img = merge_text_with_image(
+                                        img,
+                                        call_to_action_text,
+                                        description_text,
+                                        [width_percentage_cta, width_percentage_desc],
+                                        [height_percentage_cta, height_percentage_desc],
+                                        [call_to_action_text_color, description_text_color],
+                                        [call_to_action_bg_color, description_bg_color],
+                                        cta_position,
+                                        desc_position,
+                                        logo_position,
+                                        logo_width_percentage,
+                                        logo_height_percentage,
+                                        uploaded_logo
+                                    )
+
+                                    # Convert merged image to base64
+                                    buffered = BytesIO()
+                                    merged_img.save(buffered, format="PNG")
+                                    img_base64 = base64.b64encode(buffered.getvalue()).decode()
+
+                                    add_draggable_functionality(img_base64, call_to_action_text, description_text, logo_base64, img.size[0], img.size[1])
+                                    images_with_text.append(merged_img)
+
+            download_images(images_with_text, selected_image_sizes)
+            st.write("Images processed and available for download!")
 
 if __name__ == "__main__":
     main()
