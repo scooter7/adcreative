e4c3c2bde5a3683db867693c7758bcbb62703226
scooter7 <scooter.vineburgh@gmail.com>
2024-08-18 09:44:20 -0500

    Update ads5.py

--- ads5.py
+++ ads5.py
@@ -111,13 +111,13 @@ def main():
 def add_draggable_functionality(img_base64, call_to_action_text, description_text, logo_base64, img_width, img_height, cta_text_color, cta_bg_color, desc_text_color, desc_bg_color):
     st.components.v1.html(f"""
         <div id="imageContainer" style="position: relative; width: {img_width}px; height: {img_height}px; background-image: url('data:image/png;base64,{img_base64}'); background-size: contain; background-repeat: no-repeat;">
-            <div id="ctaText" style="position: absolute; top: 50px; left: 50px; cursor: move; background-color:{cta_bg_color}; color:{cta_text_color}; padding: 5px; min-width: 50px; min-height: 30px;">
+            <div id="ctaText" class="draggable resizable" style="position: absolute; top: 50px; left: 50px; cursor: move; background-color:{cta_bg_color}; color:{cta_text_color}; padding: 5px; min-width: 50px; min-height: 30px;">
                 {call_to_action_text}
             </div>
-            <div id="descText" style="position: absolute; top: 150px; left: 50px; cursor: move; background-color:{desc_bg_color}; color:{desc_text_color}; padding: 5px; min-width: 50px; min-height: 30px;">
+            <div id="descText" class="draggable resizable" style="position: absolute; top: 150px; left: 50px; cursor: move; background-color:{desc_bg_color}; color:{desc_text_color}; padding: 5px; min-width: 50px; min-height: 30px;">
                 {description_text}
             </div>
-            <div id="logoImage" style="position: absolute; top: 250px; left: 50px; cursor: move; min-width: 50px; min-height: 30px;">
+            <div id="logoImage" class="draggable resizable" style="position: absolute; top: 250px; left: 50px; cursor: move; min-width: 50px; min-height: 30px;">
                 <img src="data:image/png;base64,{logo_base64}" style="width: auto; height: auto;">
             </div>
         </div>
@@ -126,9 +126,8 @@ def add_draggable_functionality(img_base64, call_to_action_text, description_tex
 
         <script src="https://cdn.jsdelivr.net/npm/interactjs@1.10.11/dist/interact.min.js"></script>
         <script>
-            // Make elements draggable and resizable
             interact('.draggable')
-                .draggable({
+                .draggable({{
                     onmove: function (event) {{
                         var target = event.target,
                             x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -139,8 +138,8 @@ def add_draggable_functionality(img_base64, call_to_action_text, description_tex
                         target.setAttribute('data-x', x);
                         target.setAttribute('data-y', y);
                     }}
-                })
-                .resizable({
+                }})
+                .resizable({{
                     edges: {{ left: true, right: true, bottom: true, top: true }},
                     onmove: function (event) {{
                         var target = event.target,
@@ -152,7 +151,7 @@ def add_draggable_functionality(img_base64, call_to_action_text, description_tex
 
                         target.style.transform = 'translate(' + x + 'px,' + y + 'px)';
                     }}
-                });
+                }});
 
             function saveImage() {{
                 html2canvas(document.getElementById('imageContainer')).then(function(canvas) {{
@@ -163,10 +162,6 @@ def add_draggable_functionality(img_base64, call_to_action_text, description_tex
                     link.click();
                 }});
             }}
-
-            interact('#ctaText').draggable({{ inertia: true }}).resizable({{ edges: {{ left: true, right: true, bottom: true, top: true }} }});
-            interact('#descText').draggable({{ inertia: true }}).resizable({{ edges: {{ left: true, right: true, bottom: true, top: true }} }});
-            interact('#logoImage').draggable({{ inertia: true }}).resizable({{ edges: {{ left: true, right: true, bottom: true, top: true }} }});
         </script>
     """, height=img_height + 200)
 
