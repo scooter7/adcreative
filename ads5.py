dffe5032b764210b08d2c679b1b7755e115985c9
scooter7 <scooter.vineburgh@gmail.com>
2024-08-18 19:11:33 -0500

    Update ads5.py

--- ads5.py
+++ ads5.py
@@ -122,10 +122,10 @@ def add_draggable_functionality(images_data, img_width, img_height):
         # Generate HTML for each image
         html_part = f"""
             <div id="imageContainer_{index}" style="position: relative; width: {img_width}px; height: {img_height}px; background-image: url('data:image/png;base64,{data['img_base64']}'); background-size: contain; background-repeat: no-repeat;">
-                <div id="{cta_id}" class="draggable resizable" style="position: absolute; top: 50px; left: 50px; background-color:{data['cta_bg_color']}; color:{data['cta_text_color']}; padding: 5px; min-width: 50px; min-height: 30px; font-size: 16px;">
+                <div id="{cta_id}" class="draggable resizable" style="position: absolute; top: 50px; left: 50px; background-color:{data['cta_bg_color']}; color:{data['cta_text_color']}; padding: 10px; min-width: 50px; min-height: 30px; font-size: 16px;">
                     {data['call_to_action_text']}
                 </div>
-                <div id="{desc_id}" class="draggable resizable" style="position: absolute; top: 150px; left: 50px; background-color:{data['desc_bg_color']}; color:{data['desc_text_color']}; padding: 5px; min-width: 50px; min-height: 30px; font-size: 16px;">
+                <div id="{desc_id}" class="draggable resizable" style="position: absolute; top: 150px; left: 50px; background-color:{data['desc_bg_color']}; color:{data['desc_text_color']}; padding: 10px; min-width: 50px; min-height: 30px; font-size: 16px;">
                     {data['description_text']}
                 </div>
                 <div id="{logo_id}" class="draggable resizable" style="position: absolute; top: 250px; left: 50px; min-width: 50px; min-height: 30px;">
@@ -185,8 +185,15 @@ def add_draggable_functionality(images_data, img_width, img_height):
                     x = (parseFloat(target.getAttribute('data-x')) || 0),
                     y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-                target.style.width = event.rect.width + 'px';
-                target.style.height = event.rect.height + 'px';
+                target.style.width = 'auto';
+                target.style.height = 'auto';
+                target.style.whiteSpace = 'nowrap';
+
+                let newFontSize = Math.min(event.rect.width, event.rect.height) / 5;
+                target.style.fontSize = newFontSize + 'px';
+
+                // Maintain padding around text
+                target.style.padding = '10px';
 
                 x += event.deltaRect.left;
                 y += event.deltaRect.top;
@@ -195,17 +202,6 @@ def add_draggable_functionality(images_data, img_width, img_height):
 
                 target.setAttribute('data-x', x);
                 target.setAttribute('data-y', y);
-
-                if (target.id.includes('ctaText') || target.id.includes('descText')) {
-                    let newFontSize = Math.min(event.rect.width, event.rect.height) / 5;
-                    target.style.fontSize = newFontSize + 'px';
-                }
-
-                if (target.id.includes('logoImage')) {
-                    let img = target.querySelector('img');
-                    img.style.width = '100%';
-                    img.style.height = 'auto';
-                }
             }
 
             function saveImage() {
